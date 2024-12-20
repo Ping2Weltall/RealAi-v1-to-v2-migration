535b8364018f772a63a4e8704db740cdb8f20681
Alexander J. Herrmann <ping2weltall@gmail.com>
2024-12-20 15:52:50 +0100

    Aktualisieren von migration_v1_ProWords_v2.c
    Decided to add a header since it seem to be a jungle out there ..

--- migration_v1_ProWords_v2.c
+++ migration_v1_ProWords_v2.c
@@ -1,13 +1,72 @@
-#define EXPORT_TYPE   "Pro"
-#define FILE_TYPE     EXPORT_TYPE"-"
-#define TABLE_NAME    EXPORT_TYPE"Words"
-#define FIELD_NAME    EXPORT_TYPE"Word"
+/*****************************************************************************/
+/* Projekt: RuteShi                                Version  0.0.1.           */
+/*---------------------------------------------------------------------------*/
+/* Modul       : RealAI_PreWords.c                                           */
+/* Autor       : Alexander J. Herrmann                                       */
+/* Erstellt    : Dezember 2024                                               */
+/* Than you    : Instead of typing a lot myself on a Android Tablet I used   */
+/*               AI coaching. So some of the Code is not up2 industrial      */
+/*               strenght. Well, it is something to be used once - anyway:   */
+/*               Thanks to Google Gemini, MicroSoft Bing Co-pilot and        */
+/*               GitHub Co-Pilot for saving me a lot of typing.              */
+/* The Oblivonburn RealAI Version 1 can be found at GitHub:                  */
+/*      https://github.com/Oblivionburn/realai.                              */
+/* These program has it's home in the Cloud at:                              */
+/*      https://github.com/Ping2Weltall/RealAi-v1-to-v2-migration.           */
+/*...........................................................................*/
+/* Purpose:                                                                  */
+/*      Transfers the data from RealAI v1 into SQLite3 Tables                */
+/*---------------------------------------------------------------------------*/
+/* Changes:                                                                  */
+/* dd.mm.yyyy  : Author        : Modification                                */
+/*.............+...............+.............................................*/
+/*             :               :                                             */
+/*.............+...............+.............................................*/
+/* 19.12.2024  : AJH           : Added TRANSAKTION for faster processing     */
+/*---------------------------------------------------------------------------*/
+/*    THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED   */
+/*    WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF   */
+/*            MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.          */
+/*                This program is NOT FREE SOFTWARE in common!               */
+/* But as it has a dual Licence so it may still fit your needs as long as it */
+/* is used for non-profit purposes including educational use. You're also    */
+/* allowed to redistribute it and/or modify it under the included            */
+/* "LESSER GNU GENERAL PUBLIC LICENCE" Version 2.1 - see COPYING.LESSER.     */
+/* A exception is that any output like Webpages, Scripts do not automaticly  */
+/* fall under the copyright of this package, but belong to whoever generated */
+/* them and may be sold commercialy and may be aggregatet with this Software */
+/* as long as the Software itself is distributed under the Licence terms.    */
+/* C subroutines (or comparable compiled subroutines in other languages)     */
+/* supplied by you and linked into this Software in order to extend the      */
+/* functionality of this Software shall not be considered part of this       */
+/* Software and should not alter the Software in any way that would cause it */
+/* to fail the regression tests for this Software.                           */
+/* Another exception to the above Licence is that you can modify the and use */
+/* the modified Software without placing the modifications in the Public     */
+/* Domain as long as this modifications are only used within your corporation*/
+/* or organization.                                                          */
+/*---------------------------------------------------------------------------*/
+/* In case that you would like to use it in aggregate with commercial        */
+/* programs and/or as part of a larger (possibly commercial) software        */
+/* distribution than you should contact the Copyright Holder first.          */
+/* Same if you have plans which would violate one or more of the included    */
+/* "LESSER GNU GENERAL PUBLIC LICENCE" Version 2.1 Licence Terms.            */
+/*---------------------------------------------------------------------------*/
+/* (C) 2024.     Alexander Joerg Herrmann                                    */
+/*               Email:    ping2weltall@gmail.com                            */
+/*               http://fb.me/Computeralex                                   */
+/*...........................................................................*/
+/* Alle Rechte vorbehalten                               All rights reserved */
+/*****************************************************************************/
 
+// You may need to change MY_BRAIN to the directory where your RealAI Textfiles are stored
+#define EXPORT_TYPE "Pre"
+#define FILE_TYPE EXPORT_TYPE "-"
+#define TABLE_NAME EXPORT_TYPE "Words"
+#define FIELD_NAME EXPORT_TYPE "Word"
 
 #define MY_BRAIN "../Brain5511/Brain"
-// #define MY_WORDS MY_BRAIN "/Words.txt"
 #define DATABASE_NAME "../RuteShi.db"
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,8 +100,6 @@ struct stat *file_status(const char *filename);
 
 int main()
 {
-    // ... other code
-
     sqlite3 *db;
     int rc;
 
@@ -80,7 +137,8 @@ int main()
     {
         const char *sql_insert = "INSERT INTO " TABLE_NAME
                                  " (Id, Word, " FIELD_NAME ","
-                                 "  Priority, "     " Distance) VALUES (?, ?, ?, ?, ?)";
+                                 "  Priority, "
+                                 " Distance) VALUES (?, ?, ?, ?, ?)";
 
         sqlite3_stmt *stmt;
         rc = sqlite3_prepare_v2(db, sql_insert, -1, &stmt, NULL);
@@ -120,15 +178,13 @@ int dump_dir2sql(sqlite3 *db, sqlite3_stmt *stmt)
 
             if (dir->d_type == DT_REG)
             {
-                if (
-                    //(strncmp(file, "Pre-", 4) == 0))
-                    (strncmp(file, FILE_TYPE, 4) == 0))
+                if ((strncmp(file, FILE_TYPE, 4) == 0))
                 {
                     char input_file[1024];
 
                     sprintf(input_file, "%s/%s", MY_BRAIN, file);
                     printf("%5d \t", ++count);
-                    dump_file2sql(input_file, FILE_TYPE, db, stmt);
+                    rc = dump_file2sql(input_file, FILE_TYPE, db, stmt);
                 }
             }
         }
@@ -151,6 +207,7 @@ int dump_file2sql(const char *filename, char *type, sqlite3 *db, sqlite3_stmt *s
     static int idnr = 0;
     char *word = strdup(filename);
     char *sptr = (strstr(word, ".txt"));
+    char *err_msg = NULL;
     if (sptr != NULL)
     {
         *((sptr = (word + strlen(word) - 4))) = '\0';
@@ -195,36 +252,51 @@ int dump_file2sql(const char *filename, char *type, sqlite3 *db, sqlite3_stmt *s
     }
     else
     {
-
-        for (int i = 0; i < num_words; i++)
+        // Begin transaction
+        rc = sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, &err_msg);
+        if (rc != SQLITE_OK)
         {
-            int len = strlen(words[i].word);
-            printf("%0.5xd:%s:L%d:P%0.3d:D:%0.3d\n", words[i].position, words[i].word, len, words[i].priority, words[i].distance);
-            sqlite3_bind_int(stmt, 1, ++idnr);
-            sqlite3_bind_text(stmt, 2, words[i].word, -1, SQLITE_TRANSIENT);
-            sqlite3_bind_text(stmt, 3, words[i].preword, -1, SQLITE_TRANSIENT);
-            sqlite3_bind_int(stmt, 4, words[i].priority);
-            sqlite3_bind_int(stmt, 5, words[i].distance);
-
-            rc = sqlite3_step(stmt);
-            if (rc != SQLITE_DONE)
-            {
-                // Handle error
-                fprintf(stderr, "SQL error: %s Position: %d\n", sqlite3_errmsg(db), i);
-                sqlite3_close(db);
-                exit(1);
-            }
-            sqlite3_reset(stmt);
+            fprintf(stderr, "Failed to begin transaction: %s\n", err_msg);
+            sqlite3_free(err_msg);
         }
+        else
+        {
+            for (int i = 0; i < num_words; i++)
+            {
+                int len = strlen(words[i].word);
+                printf("%5d:%s:L%d:P%0.3d:D:%0.3d\n", words[i].position, words[i].word, len, words[i].priority, words[i].distance);
+                sqlite3_bind_int(stmt, 1, ++idnr);
+                sqlite3_bind_text(stmt, 2, words[i].word, -1, SQLITE_TRANSIENT);
+                sqlite3_bind_text(stmt, 3, words[i].preword, -1, SQLITE_TRANSIENT);
+                sqlite3_bind_int(stmt, 4, words[i].priority);
+                sqlite3_bind_int(stmt, 5, words[i].distance);
 
-        free(words);
+                rc = sqlite3_step(stmt);
+                if (rc != SQLITE_DONE)
+                {
+                    // Handle error
+                    fprintf(stderr, "SQL error: %s Position: %d\n", sqlite3_errmsg(db), i);
+                    sqlite3_close(db);
+                    exit(1);
+                }
+                sqlite3_reset(stmt);
+            }
+            // Commit transaction
+            rc = sqlite3_exec(db, "COMMIT", NULL, NULL, &err_msg);
+            if (rc != SQLITE_OK)
+            {
+                fprintf(stderr, "Failed to commit transaction: %s\n", err_msg);
+                sqlite3_free(err_msg);
+            }
+            free(words);
+        }
     }
     return (rc);
 }
 
 WordData *read_words(const char *filename, const char *type, char *word, int *num_words)
 {
-    static int position = 0;
+    int position = 0;
     struct stat *status = NULL;
     int size = 0;
 
@@ -262,10 +334,7 @@ WordData *read_words(const char *filename, const char *type, char *word, int *nu
                     {
                         char *preword = strtok(line, "~");
                         char *s_distance = strtok(NULL, "~");
-                        ;
-                        // char *s_distance = strtok(NULL, "~");
 
-                        //                   if (/* (preword != NULL) && */ (s_priority != NULL) /* && (s_distance != NULL) */)
                         {
                             int priority = 0;
                             int distance = 0;
@@ -298,19 +367,13 @@ WordData *read_words(const char *filename, const char *type, char *word, int *nu
                             }
                             words[count].priority = priority;
                             words[count].distance = distance;
+                            words[count].position = ++position;
                             count++;
                             printf("\t%-20s\t%d\t%d\n", words[count].preword, priority, distance);
                         }
-#if 0
-                    else
-                    {
-                        printf("Data error!\nSize: %d Data[%s]\n", size, line);
-                        exit(1);
-                    }
-#endif
                     }
                 }
-                printf("%20s\t%d]\n", word, count);
+                printf("%20s\t%d\n", word, count);
                 free(word);
                 fclose(fp);
                 *num_words = count;
@@ -340,18 +403,8 @@ struct stat *file_status(const char *filename)
 
     if (stat(filename, &fileStat) == 0)
     {
-        // printf("Letzte Änderung: %s", ctime(&fileStat.st_mtime));
-
-        n++;
-        if ((n % 1000) == 0)
-        {
-            // user_msg(M_INFO, ".");
-            if ((n % 10000) == 0)
-            {
-                // flush();
-                // user_msg(M_INFO, "\tloading ...\n");
-            }
-        }
+       // Get a segmentation fault here ..
+       // printf("Last change: %s", ctime(&fileStat.st_mtime));
     }
     else
     {
